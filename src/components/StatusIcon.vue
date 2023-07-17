8b1060cfdff27bde825a14bb82b67dad727be1e0
Ronaldo <ronaldo@iliossistemas.com>
2023-07-16 21:07:08 -0300

    Criação de componente
    1. Criado componente icon-status e estilização
    2. Criado props label e cor para o componente icon-status
    3. Criado icone RickAndMorty para o header da página

---
+++ src/components/StatusIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box">
+    <div class="ball" :style="`background-color: ${color}`" />
+    <span>{{ label }}</span>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  color: {
+    type: String,
+    default: 'grey',
+  },
+});
+</script>
+<style scoped>
+.box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: white;
+}
+.ball {
+  height: 0.5rem;
+  width: 0.5rem;
+  border-radius: 50%;
+  margin-right: 0.3rem;
+}
+</style>
