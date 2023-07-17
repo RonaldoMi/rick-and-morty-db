618e2956cbb56da574c1edc52fc643a4d667870c
Ronaldo <ronaldo@iliossistemas.com>
2023-07-16 21:03:52 -0300

    Componente g-nav
    1. Criado estilização para o componente
    2. Criado icone e inserido ao componente
    3. Finalizado componente

---
+++ src/components/generic/GNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="nav">
+    <icon-rick-nav color="rgb(51, 51, 51)" />
+  </header>
+</template>
+<script setup>
+import IconRickNav from '@/components/icons/IconRickNav.vue';
+</script>
+<style scoped>
+.nav {
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  margin: 0px auto;
+  width: 100%;
+  min-height: 60px;
+  padding: 0px 1.5rem;
+}
+</style>
