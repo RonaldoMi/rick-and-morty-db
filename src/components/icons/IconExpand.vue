9768588b8de380e30bf593e514cf0d10aebfa388
Ronaldo <ronaldo@iliossistemas.com>
2023-07-18 08:52:42 -0300

    Icones
    1. Adicionado IconExpand

---
+++ src/components/icons/IconExpand.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
+    fill="currentColor"
+  >
+    <path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z" />
+  </svg>
+</template>
