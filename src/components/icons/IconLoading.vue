5f156bbc6fef3aa2f2a6d6fc6d02836894a3cde3
Ronaldo <ronaldo@iliossistemas.com>
2023-07-17 23:20:30 -0300

    Icones
    1. Adicionado icone para de busca
    2. Adicionado icone animado para loading

---
+++ src/components/icons/IconLoading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="loading"
+    :style="`width: ${size}; height: ${size}; 
+             border-top: 4px solid ${color};`"
+  />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  size: {
+    type: String,
+    default: '24px',
+  },
+  color: {
+    type: String,
+    default: '#1867c0',
+  },
+});
+</script>
+
+<style scoped>
+.loading {
+  transform: translate(-50%, -50%);
+  border: 4px solid rgba(214, 217, 219, 0.9);
+  border-radius: 50%;
+  animation: spin 0.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style>
