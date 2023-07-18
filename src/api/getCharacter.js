e148d472aabbe68546e57dca003dcc72b1dd0784
Ronaldo <ronaldo@iliossistemas.com>
2023-07-17 23:19:49 -0300

    Apis
    1. Ajustes no stores de character e episode, para se adequar ao retorno das apis

--- src/api/getCharacter.js
+++ src/api/getCharacter.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 export const getCharacter = async (params) => {
+  // params = {page, name, status}
   const url = `https://rickandmortyapi.com/api/character`;
 
-  const response = axios.get(url, {
-    headers: {},
-    params: params,
-  });
-
+  const response = await axios
+    .get(url, {
+      headers: {},
+      params: params,
+    })
+    .catch((error) => {
+      return error.response;
+    });
   return response;
 };
--- src/api/getMultEpisode.js
+++ src/api/getMultEpisode.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 export const getEpisode = async (ids) => {
   const url = `https://rickandmortyapi.com/api/episode/${ids}`;
-  const response = axios.get(url, {
-    headers: {},
-    params: {},
-  });
+  const response = axios
+    .get(url, {
+      headers: {},
+      params: {},
+    })
+    .catch((error) => {
+      return error.response;
+    });
   return response;
 };
